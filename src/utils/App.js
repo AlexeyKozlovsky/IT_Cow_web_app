ff1112c662105e71966718e0977c966ab190f6cb
AlexeyKozlovsky <kozaa.19@uni-dubna.ru>
2021-06-23 16:52:15 +0300

    Test commit

--- src/utils/App.js
+++ src/utils/App.js
@@ -1,34 +1,10 @@
 import '../utils/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NaviBar from '../components/Navibar';
-import MyCard from '../components/Mycard'
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from '../components/AppRouter';
 
-import firebase from '../firebase';
-
-
 function App() {
-
-  // var database = firebase.database();
-
-  // const dbRef = firebase.database().ref();
-  // dbRef.child("cards").get().then((snapshot) => {
-  //   if (snapshot.exists()) {
-  //     console.log(snapshot.val());
-  //   } else {
-  //     console.log("No data available");
-  //   } 
-  // }).catch((error) => {
-  //     console.error(error);
-  // });
-
-
-  // const ref = firebase.firestore().collection("cards");
-  // console.log(ref);
-
-
-
   return (
     <div>
       <BrowserRouter className="App page">
